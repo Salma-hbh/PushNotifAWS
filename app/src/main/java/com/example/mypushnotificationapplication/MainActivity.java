c6db086d9780de9ac74f78d0a2c498dddb23caab
salma-hbh <elhabhoubsalma2@gmail.com>
2023-02-13 16:17:59 +0100

    second push

---
+++ app/src/main/java/com/example/mypushnotificationapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mypushnotificationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.amazon.device.messaging.ADM;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etToken;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etToken = findViewById(R.id.etToken);
+        boolean ADMAvailable = false;
+        try {
+            Class.forName("com.amazon.device.messaging.ADM");
+            ADMAvailable = true;
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        if (ADMAvailable) {
+            final ADM adm = new ADM(this);
+            if (adm.getRegistrationId() == null) {
+                // startRegister() is asynchronous; your app is notified via the
+                // onRegistered() callback when the registration ID is available.
+                adm.startRegister();
+                Toast.makeText(MainActivity.this, "Your reg id is" + adm.getRegistrationId(), Toast.LENGTH_SHORT).show();
+                etToken.setText(adm.getRegistrationId());
+            }
+            
+
+        }
+    }
+}
